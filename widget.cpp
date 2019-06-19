59ced3ef2b1fe2f559355f9f58927cf4c3045261
eeennn <1275994522@qq.com>
2019-06-19 10:57:52 +0800

    Update widget.cpp

--- widget.cpp
+++ widget.cpp
@@ -16,9 +16,9 @@ Widget::Widget(QWidget *parent) :
 
     player = new QMediaPlayer(this);
     videoWidget = new QVideoWidget(this);
-    videoWidget->resize(600,300);
+    videoWidget->resize(600,300);//设置程序窗口
     player->setVideoOutput(videoWidget);
-    //player->setMedia(QUrl::fromLocalFile("C:/Users/ASUS/Desktop/11.mp4"));
+    //player->setMedia(QUrl::fromLocalFile("C:/Users/ASUS/Desktop/11.mp4"));//直接播放11.mp4
 
     connect(player,&QMediaPlayer::positionChanged,this,&Widget::updatePosition);
 
@@ -42,9 +42,9 @@ Widget::Widget(QWidget *parent) :
     connect(videoWidget,&QVideoWidget::saturationChanged,ui->horizontalSlider_5,&QSlider::value);
 
     model = new QStandardItemModel(this);
-    strListFileName.clear();//文件名称对应的列表
+    strListFileName.clear();//文件名称对应的列表，清理
     strListUrlName.clear();
-    ui->listView->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->listView->setEditTriggers(QAbstractItemView::NoEditTriggers);//设置不编辑
 }
 
 Widget::~Widget()
@@ -55,12 +55,12 @@ Widget::~Widget()
 void Widget::updatePosition(qint64 position){
     //更新滑块显示并更新时间
     ui->horizontalSlider->setMaximum(player->duration()/1000);
-    ui->label_3->setText(ChangeTime(player->duration()/1000));
+    ui->label_3->setText(ChangeTime(player->duration()/1000));//总时长
     ui->label->setText(ChangeTime(position/1000));
-    ui->horizontalSlider->setValue(position/1000);
+    ui->horizontalSlider->setValue(position/1000);//滑条根据时间移动
 }
 
-QString Widget::ChangeTime(qint64 position){
+QString Widget::ChangeTime(qint64 position){//时间转换，position是总时长，计算时分秒
     int min = position/60;
     int second = position%60;
     int hour = 0;
@@ -92,29 +92,29 @@ void Widget::on_pushButton_3_clicked()
 void Widget::on_pushButton_4_clicked()
 {
     //qDebug()<<"点击打开文件";
-    QString filename = QFileDialog::getOpenFileName(this,"open Video",QDir::currentPath(),"video file(*.mp4);;ALL file(*.*)");
+    QString filename = QFileDialog::getOpenFileName(this,"open Video",QDir::currentPath(),"video file(*.mp4);;ALL file(*.*)");//找到路径，然后播放
     player->setMedia(QUrl::fromLocalFile(filename));
     player->play();
 }
 
 void Widget::on_pushButton_5_clicked()
 {
-    qDebug()<<"点击打开目录";
+    qDebug()<<"点击打开目录";//调试输出代码
     strListFileName.clear();//文件名称对应的列表
-    strListUrlName.clear();
+    strListUrlName.clear();//文件列表显示清除
     model->clear();
     player->stop();
 
-    QString dirname = QFileDialog::getExistingDirectory(this,tr("open Directiry"),".");
+    QString dirname = QFileDialog::getExistingDirectory(this,tr("open Directiry"),".");//获取打开的路径
     QDir dir(dirname);
-    dir.setNameFilters(QStringList("*.mp4"));
+    dir.setNameFilters(QStringList("*.mp4"));//设置打开MP4格式文件
     strListFileName = dir.entryList();//获取文件列表
     //qDebug()<<strListFileName;
     QString strPathName = dir.absolutePath();//获取目录路径
     qDebug()<<"strPathName:"+strPathName;
     int count = strListFileName.count();
 
-    for(int i=0;i<count;i++){
+    for(int i=0;i<count;i++){                   //将文件和路径合并，保存成列表形式即数组
         QStandardItem *item = new QStandardItem(strListFileName.at(i));
         model->appendRow(item);
         strListUrlName.append(strPathName+"/"+strListFileName.at(i));
@@ -130,11 +130,11 @@ void Widget::on_listView_doubleClicked(const QModelIndex &index)
     //qDebug()<<"双击列表";
     player->setMedia(QUrl::fromLocalFile(strListUrlName.at(index.row())));
     qDebug()<<"strListUrlName:"+strListUrlName.at(index.row());
-    player->play();
+    player->play();//播放
 }
 
-void Widget::on_horizontalSlider_sliderMoved(int position)
+void Widget::on_horizontalSlider_sliderMoved(int position)//滑条移动
 {
     //设置播放进度
-    player->setPosition(position);
+    player->setPosition(position);//根据时间移动
 }
